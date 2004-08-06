30aaef19d20e57135fefdc188e850bcbf3f863b3
Ludwig Nussel <ludwig.nussel@suse.de>
2004-08-06 13:21:37 +0000

    new
    git-svn-id: https://forgesvn1.novell.com/svn/susefirewall2/trunk@38 b36d0de6-17df-0310-aa5c-c2ebc275e154

---
+++ Makefile
@@ -0,0 +1,46 @@
+VERSION=3.2
+NAME=SuSEfirewall2
+NVER=$(NAME)-$(VERSION)
+ARCHIVE=$(NVER).tar.gz
+
+DESTDIR=
+
+allfiles=SuSEfirewall2_final \
+	SuSEfirewall2_init \
+	SuSEfirewall2_setup \
+	SuSEfirewall2-autointerface.sh \
+	SuSEfirewall2-custom.sysconfig \
+	SuSEfirewall2 \
+	sysconfig-personal-firewall \
+	SuSEfirewall2.sysconfig \
+	Makefile \
+	EXAMPLES \
+	FAQ \
+	LICENCE \
+	README
+
+tar: $(ARCHIVE)
+
+$(ARCHIVE): $(allfiles)
+	rm -rf $(NVER)
+	mkdir $(NVER)
+	for i in $(allfiles); do \
+		ln $$i $(NVER)/$$i; \
+	done
+	tar --owner=root --group=root -czf $(ARCHIVE) $(NVER)
+	rm -rf $(NVER)
+
+install:
+	install -d -m 755 $(DESTDIR)/sbin
+	install -d -m 755 $(DESTDIR)/etc/init.d
+	install -d -m 755 $(DESTDIR)/etc/sysconfig/scripts
+	install -m 755 SuSEfirewall2 $(DESTDIR)/sbin
+	install -m 755 SuSEfirewall2_init $(DESTDIR)/etc/init.d
+	install -m 755 SuSEfirewall2_setup $(DESTDIR)/etc/init.d
+	install -m 755 SuSEfirewall2_final $(DESTDIR)/etc/init.d
+	ln -s /etc/init.d/SuSEfirewall2_setup $(DESTDIR)/sbin/rcSuSEfirewall2
+
+clean:
+	rm -f $(ARCHIVE)
+
+.PHONY: clean
---
+++ SuSEfirewall2-autointerface.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+#
+# determine which network devices are internal and which are external
+#
+# The external device is always the one where the default route points at. If
+# there is no default route, then there is also no external device.
+#
+# Active devices except the external one are considered as candidates for
+# internal. Devices that are configured for pppoe in
+# /etc/sysconfig/network/ifcfg-dsl* are removed from the active list. If only
+# one device is left after that filter, it's considered as internal.
+#
+# => only one external and one internal possible
+# => if you only have one device with no default route it's internal
+#
+# All packets that arrive on devices that are neither internal nor external
+# will be dropped by the firewall
+
+# print the device where the default route points at
+get_default_route_dev()
+{
+	while read line; do
+		set -- $line
+		[ "$1" != default ] && continue;
+		# interface name comes after a "dev" token
+		while [ "$1" != dev -a $# -gt 0 ]; do shift; done
+		if [ "$1" = dev ]; then
+			echo $2
+			break;
+		fi
+	done < <(ip route show)
+}
+
+# print active interfaces except lo
+get_active_interfaces()
+{
+	while read line; do
+		set -- $line
+		case "$3" in
+			*UP*)
+				dev=${2%%:}
+				[ "$dev" != "lo" ] && echo $dev
+			;;
+		esac
+	done < <(ip -o link show)
+}
+
+# first parameter is device to filter from rest of arguments
+filter_one_dev()
+{
+	filter="$1"
+	shift
+	if [ -z "$filter" ]; then
+		echo "$@"
+		return;
+	fi
+	for i in "$@"; do
+		[ "$filter" = "$i" ] && continue
+		echo $i
+	done
+}
+
+# filter devices for which a pppoe link is configured. exit with status 1 if
+# more than one device is left
+filter_pppoe_devs()
+{
+	for i in /etc/sysconfig/network/ifcfg-dsl*; do
+		. $i
+		[ -z "$DEVICE" -o $PPPMODE != pppoe ] && continue
+		if [ -x "/sbin/getcfg-interface" ] && ! ip link show dev "$DEVICE" > /dev/null 2>&1; then
+			DEVICE=`/sbin/getcfg-interface "$DEVICE"` || continue
+		fi
+		set -- `filter_one_dev "$DEVICE" "$@"`
+	done
+	echo "$@"
+	[ "$#" -gt 1 ] && return 1
+	return 0
+}
+
+shopt -s nullglob
+
+internal=
+external=`get_default_route_dev`
+
+# all active devices
+active=`get_active_interfaces`
+
+# active devices except the default route device
+filtered=`filter_one_dev "$external" $active`
+
+# active devices minus pppoe devices
+filtered2=`filter_pppoe_devs $filtered`
+[ "$?" = 0 ] && internal=$filtered2
+
+echo "External: $external"
+echo "Internal: $internal"
+
+#echo "Active: $active"
+#echo "Filtered: $filtered"
+#echo "Filtered2: $filtered2"
