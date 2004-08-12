6b10655213c982b0df6b36fce98828eb89cf8dbc
Ludwig Nussel <ludwig.nussel@suse.de>
2004-08-12 12:27:35 +0000

    add gpl header
    git-svn-id: https://forgesvn1.novell.com/svn/susefirewall2/trunk@41 b36d0de6-17df-0310-aa5c-c2ebc275e154

--- SuSEfirewall2-autointerface.sh
+++ SuSEfirewall2-autointerface.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 #
+# SuSEfirewall2-autointerface.sh - helper script for SuSEfirewall2
+# Copyright (C) 2004 SUSE Linux AG
+#
+# Author: Ludwig Nussel
+# 
+# Please send feedback via http://www.suse.de/feedback
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 # determine which network devices are internal and which are external
 #
 # The external device is always the one where the default route points at. If
